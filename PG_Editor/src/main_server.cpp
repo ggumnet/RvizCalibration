5ff9c2316d25b27e06598ed8e1da432b05eabc24
rideflux <ggumnet@rideflux.com>
2022-08-05 16:17:50 +0900

    graph twisted

--- PG_Editor/src/main_server.cpp
+++ PG_Editor/src/main_server.cpp
@@ -325,22 +325,36 @@ void requestRelativeFactor(int source_time_step, int dest_time_step)
 {
     pg_editor::GetNDTMatchingResult matching_result_service;
     Transform T_source, T_dest;
+    pointcloud_tools::SensorDataID id_source, id_dest;
 
-    T_source = transform_vec_.at(source_time_step);
-    T_dest = transform_vec_.at(dest_time_step);
+    id_source = id_dest = init_id;
+    id_source.time_step = source_time_step;
+    id_dest.time_step = dest_time_step;
+
+    // T_source = transform_vec_.at(source_time_step);
+    // T_dest = transform_vec_.at(dest_time_step);
+
+    Pose::Ptr pose_source = (*graph_ptr).getVariable<Pose>(id_source, true);
+    Pose::Ptr pose_dest = (*graph_ptr).getVariable<Pose>(id_dest, true);
+
+    T_source.setTranslation((*pose_source).getData().getTranslation());
+    T_source.setRotation((*pose_source).getData().getRotation());
+    T_dest.setTranslation((*pose_dest).getData().getTranslation());
+    T_dest.setRotation((*pose_dest).getData().getRotation());
+
 
     matching_result_service.request.pointcloud1 = pointcloud_vec_.at(source_time_step);
     matching_result_service.request.pointcloud2 = pointcloud_vec_.at(dest_time_step);
     matching_result_service.request.time_step1 = source_time_step;
     matching_result_service.request.time_step2 = dest_time_step;
-    //ROS_WARN("pc size print");
-    //ROS_INFO("%d %d", matching_result_service.request.pointcloud1.data.size(), matching_result_service.request.pointcloud2.data.size());
     matching_result_service.request.initial_pose = transformToPose(T_source.inv() * T_dest);
 
     if (matching_result_client.call(matching_result_service))
     {
         responseRelativeFactor(matching_result_service);
     }
+    //ROS_WARN("pc size print");
+    //ROS_INFO("%d %d", matching_result_service.request.pointcloud1.data.size(), matching_result_service.request.pointcloud2.data.size());
 }
 
 void visualizePointclouds()
@@ -519,7 +533,7 @@ int main(int argc, char **argv)
     tf::TransformBroadcaster broadcaster;
     tf::TransformBroadcaster sensor_broadcaster;
 
-    //addEdges();
+    addEdges();
 
     while (ros::ok())
     {
